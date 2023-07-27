6ba429f4ce6b288f87dde0bd8201bb95dc9fe749
LavakeshonS <140726215+LavakeshonS@users.noreply.github.com>
2023-07-27 13:36:39 -0400

    init commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/**
+ * WEB222 – Assignment Final
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Lavakeshon Selvarajah
+ *      Student ID: 106393150
+ *      Date:       December 12, 2022
+ */
+function appear() {
+    const hire = document.getElementById("rateDisplay");
+    hire.style.display = "block";
+}
+function disappear() {
+    const hire = document.getElementById("rateDisplay");
+    hire.style.display = "none";
+}
+// function getFocus() {
+// document.getElementById("fname").focus(); 
+// }
